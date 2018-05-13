d62e5af78e91a4df2aa0ab8d0a957dd112530e01
Edward Villegas-Pulgarin <cosmoscalibur@gmail.com>
2018-05-13 17:40:44 -0500

    Expose new methods in moodle math module.

--- moodlesg/math/__init__.py
+++ moodlesg/math/__init__.py
@@ -1,5 +1,26 @@
+"""
+Module :code:`moodlesg.math` implements a python interface to math manipulation
+requiered in grade_ and answer_ moodle formulas.
+
+You can use variable assigment, relation operators, mixing types (moodle
+expressions with python data types) and commons names of functions in both type
+of formulas, grade and answers.
+
+References
+----------
+
+.. [grade] `Grade calculations <https://docs.moodle.org/34/en/Grade_calculations>`_.
+            Moodle 3.4 Docs.
+
+.. [answer] `Calculated question type <https://docs.moodle.org/34/en/Calculated_question_type>`_
+             Moodle 3.4 Docs.
+"""
+
 from moodlesg.math.base import Expression, grade_var, answer_var
-from moodlesg.math.constants import pi
+from moodlesg.math.constants import PI
 from moodlesg.math.functions.common import (max, min, average, sum, floor,
-                                            ceil, round, abs, power, mod)
+                                            ceil, round, abs, power, mod,
+                                            sin, cos, tan, asin, acos, atan,
+                                            sinh, cosh, tanh, asinh, acosh,
+                                            atanh, sqrt, exp)
 from moodlesg.math.functions.extended import trunc, bool
