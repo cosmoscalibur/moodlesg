72e041ed0b4d5f9f16597fd654efba2dc45094ae
Edward Villegas-Pulgarin <cosmoscalibur@gmail.com>
2018-05-13 15:56:22 -0500

    Best docstring in constants module and caps convention fixed.

--- moodlesg/math/constants/common.py
+++ moodlesg/math/constants/common.py
@@ -1,12 +1,17 @@
 """
-:code:`moodlesg.math.constants.common` define common math moodle constants
-between grade and answer calculations.
+Module :code:`constants` define common math moodle constants between grade and
+answer calculations.
+
+In Moodle, actually there is no predefined constant that is allowed other than
+pi() as a function without parameter. With this module, is posible use
+:math:`\\pi` as a constant (not call to a function) and define others
+constants.
 """
 from moodlesg.math.base import Expression
 
-__all__ = ['pi']
+__all__ = ['PI']
 
-pi = Expression("pi()")
+PI = Expression("pi()")
 """
 Define :math:`\\pi`.
 """
